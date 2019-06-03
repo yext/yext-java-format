bd7a4ce428849816d9efda087921f37110289ee6
Matt Bloom <mbloom@yext.com>
2019-06-03 18:41:14 -0400

    Refactored FormatterUtil to load a jar dynamically.
    This change reads a jar from a set path in alpha to format text
    input. In addition, dependencies on com.google.googlejavaformat.java
    have been removed.

--- idea_plugin/src/com/google/googlejavaformat/intellij/FormatterUtil.java
+++ idea_plugin/src/com/google/googlejavaformat/intellij/FormatterUtil.java
@@ -18,48 +18,87 @@ package com.google.googlejavaformat.intellij;
 
 import static com.google.common.base.Preconditions.checkState;
 
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
 import com.google.common.collect.BoundType;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Range;
-import com.google.googlejavaformat.java.Formatter;
-import com.google.googlejavaformat.java.FormatterException;
 import com.intellij.openapi.util.TextRange;
-import java.util.Collection;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 final class FormatterUtil {
 
-  private FormatterUtil() {}
-
-  static Map<TextRange, String> getReplacements(
-      Formatter formatter, String text, Collection<TextRange> ranges) {
-    try {
-      ImmutableMap.Builder<TextRange, String> replacements = ImmutableMap.builder();
-      formatter
-          .getFormatReplacements(text, toRanges(ranges))
-          .forEach(
-              replacement ->
-                  replacements.put(
-                      toTextRange(replacement.getReplaceRange()),
-                      replacement.getReplacementString()));
-      return replacements.build();
-    } catch (FormatterException e) {
-      return ImmutableMap.of();
+    private FormatterUtil() {
     }
-  }
 
-  private static Collection<Range<Integer>> toRanges(Collection<TextRange> textRanges) {
-    return textRanges
-        .stream()
-        .map(textRange -> Range.closedOpen(textRange.getStartOffset(), textRange.getEndOffset()))
-        .collect(Collectors.toList());
-  }
+    static Map<TextRange, String> getReplacements(String text, Collection<TextRange> ranges) {
+        try {
+            ImmutableMap.Builder<TextRange, String> replacements = ImmutableMap.builder();
 
-  private static TextRange toTextRange(Range<Integer> range) {
-    checkState(
-        range.lowerBoundType().equals(BoundType.CLOSED)
-            && range.upperBoundType().equals(BoundType.OPEN));
-    return new TextRange(range.lowerEndpoint(), range.upperEndpoint());
-  }
+            String jarPath = String.format(
+                "%s/devops/format/yext-java-formatter.jar",
+                System.getenv("ALPHA"));
+            File formatterJar = new File(jarPath);
+            ClassLoader classLoader = URLClassLoader.newInstance(
+                new URL[]{
+                    formatterJar.toURI().toURL()
+                });
+
+            Class<?> formatterClass = Class.forName("com.google.googlejavaformat.java.Formatter", true, classLoader);
+            Class<?> replacementClass = Class.forName("com.google.googlejavaformat.java.Replacement", true, classLoader);
+            Class<?> rangeClass = Class.forName("com.google.common.collect.Range", true, classLoader);
+
+            Object formatter = formatterClass.newInstance();
+            Method getFormatReplacements = formatterClass.getMethod("getFormatReplacements", String.class, Collection.class);
+            Object replacementsList = getFormatReplacements.invoke(formatter, text, toRanges(ranges, classLoader));
+            for (Object replacement : (Iterable) replacementsList) {
+                // The formatter jar contains it's own copy of the Range class, resulting in
+                // duplicate but not equal classes. Therefore the Range objects returned from
+                // methods invoked by the class loader will need to be cast to the correct class.
+                Object rangeObj = replacementClass.getMethod("getReplaceRange").invoke(replacement);
+                Range<Integer> range = Range.closedOpen(
+                    (Integer) rangeClass.getMethod("lowerEndpoint").invoke(rangeObj),
+                    (Integer) rangeClass.getMethod("upperEndpoint").invoke(rangeObj));
+
+                replacements.put(
+                    toTextRange(range),
+                    (String) replacementClass.getMethod("getReplacementString").invoke(replacement));
+            }
+            return replacements.build();
+        } catch (InvocationTargetException | MalformedURLException | ClassNotFoundException | IllegalAccessException | InstantiationException | NoSuchMethodException e) {
+            return ImmutableMap.of();
+        }
+    }
+
+    private static Collection<Object> toRanges(
+        Collection<TextRange> textRanges,
+        ClassLoader loader)
+    throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<?> rangeClass = Class.forName("com.google.common.collect.Range", true, loader);
+        Method closedOpen = rangeClass.getMethod("closedOpen", Comparable.class, Comparable.class);
+
+
+        List<Object> ranges = new ArrayList<>();
+
+        for (TextRange textRange : textRanges) {
+            ranges.add(closedOpen.invoke(null, textRange.getStartOffset(), textRange.getEndOffset()));
+        }
+
+        return ranges;
+    }
+
+    private static TextRange toTextRange(Range<Integer> range) {
+        checkState(
+            range.lowerBoundType().equals(BoundType.CLOSED)
+                && range.upperBoundType().equals(BoundType.OPEN));
+        return new TextRange(range.lowerEndpoint(), range.upperEndpoint());
+    }
 }
